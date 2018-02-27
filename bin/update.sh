2ac3b64adc22c02b94bd56cbdbbb81389f7a535e
Adam Shaw <arshaw@users.noreply.github.com>
2018-02-26 23:29:54 -0500

    enforce param

--- bin/update.sh
+++ bin/update.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #
 # Builds the jekyll site in a portable manner.
-# Accepts one param: production OR development (default)
+# Accepts one param: production OR development
 #
 set -e # always immediately exit upon error
 cd "`dirname $0`/.." # start in project root
@@ -11,11 +11,15 @@ bundler install
 
 npm install
 
-if [[ "$1" ]]
+if [[ "$1" == 'production' ]]
 then
   echo 'Building as PRODUCTION'
   JEKYLL_ENV="$1" bundle exec jekyll build
-else
+elif [[ "$1" == 'development' ]]
+then
   echo 'Building as DEVELOPMENT'
   bundle exec jekyll build
+else
+  echo 'Need to specify either "production" or "development" as single arg.'
+  exit 1
 fi
