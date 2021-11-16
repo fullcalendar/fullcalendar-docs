d320c4ae68e229f1329ad766fc7e6299decbb957
Adam Shaw <arshaw@users.noreply.github.com>
2021-11-16 17:32:03 -0500

    better parent_baseurl behavior when in dev-mode

--- _plugins/site_dynamic_data.rb
+++ _plugins/site_dynamic_data.rb
@@ -3,7 +3,7 @@ class SiteDataDynamic < Jekyll::Generator
 
   def generate(site)
     site.data['parent_baseurl'] =
-      if File.directory?(__dir__ + '/../../api')
+      if ENV['JEKYLL_ENV'] === 'build' and File.directory?(__dir__ + '/../../api')
         site.config['local_parent_baseurl']
       else
         site.config['live_parent_baseurl']
